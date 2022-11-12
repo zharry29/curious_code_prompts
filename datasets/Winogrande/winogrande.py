4e0cf2db29bbf388d6716797517093a77b884a8e
Seacow <46582003+SeacowX@users.noreply.github.com>
2022-11-12 09:58:22 -0500

    update winogrande

--- datasets/Winogrande/winogrande.py
+++ datasets/Winogrande/winogrande.py
@@ -8,7 +8,8 @@ from tqdm import tqdm
 from datasets import load_dataset
 from sklearn.metrics import accuracy_score
 
-openai.api_key_path = '../../_private/{args.key}.key'
+from codex_prompts import CodexPrompts
+
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 random.seed(29)
 
@@ -26,15 +27,18 @@ class Winogrande():
             text_prompt += input_text + '\n\nAnswer: ' + output_text + '\n\n\n'
         return(text_prompt)
     
-    def build_code_prompt(self):
-        code_prompt = ""
+    def build_code_prompt(self, style, train=True):
+        text_prompt = self.build_text_prompt()
+        text_prompt_lst = [s for s in text_prompt.split('\n') if s]
+        codex_prompt = CodexPrompts(text_prompt_lst, train)
+        code_prompt = eval('codex_prompt.' + str(style))()
         return code_prompt
     
     def run_llm(self, prompt, model, temperature=0, stop=['\n']):
         model_name = {
             "davinci": "text-davinci-002",
             "curie": "text-curie-001",
-            "codex": "codex-davinci-002",
+            "codex": "code-davinci-002",
         }
         while True:
             try:
@@ -61,15 +65,24 @@ class Winogrande():
         if args.prompt == "text":
             prompt = self.build_text_prompt()
         elif args.prompt == "code":
-            prompt = self.build_code_prompt()
+            prompt = self.build_code_prompt(args.style)
 
         preds = []
         golds = []
         c = 0
         for example in tqdm(dataset['validation']):
             input_text, output_text = self.template.apply(example)
+            
             # print(prompt + input_text + '\n\nAnswer:')
-            pred = self.run_llm(prompt + input_text + '\n\nAnswer:', args.model)
+            if args.model == 'text':
+                pred = self.run_llm(prompt + input_text + '\n\nAnswer:', args.model)
+            else:
+                codex_prompt = CodexPrompts(input_text.split('\n'), train=False)
+                inference_prompt = eval('codex_prompt.' + str(args.style))()
+            prompt += inference_prompt
+            pred = self.run_llm(prompt, args.model)
+            print(prompt + pred)
+            raise SystemExit()
             gold = example['answer']
             preds.append(pred)
             golds.append(gold)
@@ -91,6 +104,7 @@ class Winogrande():
 parser = argparse.ArgumentParser()
 parser.add_argument('--prompt', type=str, help='Either text or code.')
 parser.add_argument('--model', type=str, help='Either davinci, curie or codex.')
+parser.add_argument('--style', type=str, help='choose style of code prompt from one of ["vanilla", "good_var_name", "with_comments", "class_obj"]')
 #parser.add_argument('--dataset', type=str, help='Name of the datasset')
 #parser.add_argument('--xxx', action='store_true', help='')
 parser.add_argument('--key', type=str, help='The name of the OpenAI API key file.')
@@ -98,12 +112,23 @@ parser.add_argument('--key', type=str, help='The name of the OpenAI API key file
 
 if __name__ == '__main__':
     args = parser.parse_args()
+    openai.api_key_path = f'../../_private/{args.key}.key'
 
     data_name = 'winogrande'
     NUM_EXAMPLES_IN_PROMPT = 5
 
     dataset, templates = utils.load_data(data_name)
     inference_model = Winogrande(templates)
+
+    # # !============ Code in development ===============
+    # if args.prompt == 'text':
+    #     test = inference_model.build_text_prompt()
+    # elif args.prompt == 'code':
+    #     test = inference_model.build_code_prompt(args.style)
+    # print(test)
+    # raise SystemExit()
+    # # !============ Code in development ===============
+    
     inference_model.predict()
     inference_model.evaluate()
 
