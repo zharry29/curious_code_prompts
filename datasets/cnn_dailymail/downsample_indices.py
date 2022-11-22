a210a3bea414bc86183b9d071454a3c205d1e5e4
Liam P Dugan <ldugan@seas.upenn.edu>
2022-11-22 17:53:22 -0500

    Updated CNN DailyMail script

---
+++ datasets/cnn_dailymail/downsample_indices.py
@@ -0,0 +1,8 @@
+from datasets import load_dataset
+import random
+random.seed(29)
+
+dataset = load_dataset("cnn_dailymail", "3.0.0", cache_dir="/nlp/data/huggingface_cache")
+example_indices = random.sample(range(len(dataset['validation'])), 1000)
+with open("sampled_1000_indices.txt","w+") as f:
+    f.write("\n".join(map(str, example_indices)))
